29e04b24b8298f0dd6f13a957a53e398b01d6b88
YunRuWang68 <2624718595@qq.com>
2025-11-11 20:12:24 +0800

    add basic_test.sql for basic test

--- test/basic_test.sql
+++ test/basic_test.sql
@@ -1,12 +1,4 @@
--- 创建表
-create table t1(id int, name char(10));
-
--- 插入数据
-insert into t1 values(1, 'Alice');
-insert into t1 values(2, 'Bob');
-
--- 查询数据
-select * from t1;
-
--- 删除表
-drop table t1;
+create table t(id int, name char(10));
+insert into t values(1, 'Alice');
+select * from t;
+drop table t;
