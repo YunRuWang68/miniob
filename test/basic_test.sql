b42e94692b5de22a0020af3e82281e49caf464f7
YunRuWang68 <2624718595@qq.com>
2025-11-11 20:00:50 +0800

    添加 basic_test.sql 测试文件

---
+++ test/basic_test.sql
@@ -0,0 +1,12 @@
+-- 创建表
+create table t1(id int, name char(10));
+
+-- 插入数据
+insert into t1 values(1, 'Alice');
+insert into t1 values(2, 'Bob');
+
+-- 查询数据
+select * from t1;
+
+-- 删除表
+drop table t1;
